cb4c1c6cc6aeb4576db49e216a461cdd71d67a09
rinditriandi <rindi.triandi@gmail.com>
2019-07-19 09:35:52 +0700

    Add Locations Index

--- qhse_monitoring/apps/locations/views.py
+++ qhse_monitoring/apps/locations/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {
+        'title': 'Lokasi'
+    }
+
+    return render(request, 'locations/index.html', context)
