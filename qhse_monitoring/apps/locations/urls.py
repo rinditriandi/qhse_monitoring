8ef4331ff2f30ce2705dbb987c1b0d6fc3a5d3cd
rinditriandi <rindi.triandi@gmail.com>
2019-07-19 11:15:39 +0700

    Edit Locations

--- qhse_monitoring/apps/locations/urls.py
+++ qhse_monitoring/apps/locations/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import index, add
+from .views import index, add, edit
 
 app_name = 'locations'
 
 urlpatterns = [
     path('', index, name='index'),
     path('add/', add, name='add'),
+    path('edit/<int:id>/', edit, name='edit'),
 ]
